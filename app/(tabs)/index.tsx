4934dc39d1a7cc328d7b73b20b072286b1cee824
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-03-28 22:45:30 +0600

    created styles for home & settings (i hate it)

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,38 @@
-import useTheme from "@/hooks/useTheme";
+import useTheme, { ColorScheme } from "@/hooks/useTheme";
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
+// import { useQuery } from "convex/react";
+// import { api } from "@/convex/_generated/api";
 
 const Index = () => {
-  const { toggleTheme } = useTheme();
+    const { toggleTheme, colors } = useTheme();
+    const styles = createStyles(colors);
+
     return (
         <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
+            style={styles.container}
         >
-            <Text>Index</Text>
+            <Text style={styles.text}>Index</Text>
             <TouchableOpacity onPress={toggleTheme}>
-                <Text>Toggle Theme</Text>
+                <Text style={styles.text}>Toggle Theme</Text>
             </TouchableOpacity>
         </View>
     );
+};
+
+const createStyles = (colors: ColorScheme) => {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: colors.bg,
+    },
+    text: {
+      color: colors.text,
+    },
+  });
+  return styles;
 };
 
 export default Index;
