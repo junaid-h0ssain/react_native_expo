3669f935fdfe3a001c41026594383bd7d5a60f0c
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-06-12 03:29:24 +0600

    expenso mobile init setup

--- expenso/mobile/app/_layout.tsx
+++ expenso/mobile/app/_layout.tsx
@@ -1,5 +1,22 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import SafeScreen from "@/components/SafeScreen";
+import { ClerkProvider } from "@clerk/expo";
+import { tokenCache } from "@clerk/expo/token-cache";
+import { StatusBar } from "expo-status-bar";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
+      <StatusBar style="dark" />
+    </ClerkProvider>
+  );
 }
