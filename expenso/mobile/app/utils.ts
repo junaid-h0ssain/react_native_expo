feeeffd6cab71cc8f1570ac145c063279e155c0d
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-06-12 03:31:12 +0600

    date formatter

---
+++ expenso/mobile/app/utils.ts
@@ -0,0 +1,10 @@
+// utils.ts
+export function formatDate(dateString) {
+
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
