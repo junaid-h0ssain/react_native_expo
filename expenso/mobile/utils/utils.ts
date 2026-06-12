83e67342d4fe0ea3ed7d35f1ff1aa750232e2f1b
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-06-13 00:50:02 +0600

    structure new

---
+++ expenso/mobile/utils/utils.ts
@@ -0,0 +1,10 @@
+export const formatDate = (dateString: string | number | Date) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
+
+export const API_URL = "http://localhost:5001/api";
