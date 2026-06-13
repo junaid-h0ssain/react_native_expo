12c28306654e409b0c91d50ef4f1a35cf5b70794
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-06-13 21:52:49 +0600

    subly nativewin setup done, big pain in butt

--- subly/subly-mobile/app/index.tsx
+++ subly/subly-mobile/app/index.tsx
@@ -1,10 +1,10 @@
-import "../global.css"
+import "@/global.css"
 import { Text, View } from "react-native";
  
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-xl font-bold text-blue-600">
         Welcome to Nativewind!
       </Text>
     </View>
--- subly/subly-mobile/tailwind.config.js
+++ subly/subly-mobile/tailwind.config.js
@@ -1,13 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./App.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
-  ],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+export const content = ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"];
+export const presets = [require("nativewind/preset")];
+export const theme = {
+  extend: {},
 };
+export const plugins = [];
